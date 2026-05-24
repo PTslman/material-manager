240764ca1b6f871c0f9100d018a2ed0701a93036
PT slman <slman2233445566y@gmail.com>
2026-05-24 16:49:46 +0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,1 +1,22 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDQbf5LJRCquRsheFYqvEQBQbI_EoXNOFw",
+    authDomain: "abo-slman.firebaseapp.com",
+    projectId: "abo-slman",
+    storageBucket: "abo-slman.firebasestorage.app",
+    messagingSenderId: "874996942668",
+    appId: "1:874996942668:web:f31da5ca778fb92845f1e9"
+};
 
+if (!firebase.apps.length) {
+    firebase.initializeApp(firebaseConfig);
+}
+
+const db = firebase.firestore();
+db.settings({ 
+    experimentalForceLongPolling: true, 
+    ignoreUndefinedProperties: true 
+});
+
+const materialsCollection = db.collection("spices_final_v12");
+
+console.log('✅ Firebase initialized');
